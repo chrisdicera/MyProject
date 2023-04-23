5b513555d4819d9623c10242db58444d6cafd087
chrisdicera <96492784+chrisdicera@users.noreply.github.com>
2023-04-23 15:09:29 -0400

    progress

--- LinkedList.java
+++ LinkedList.java
@@ -5,12 +5,15 @@ public class LinkedList {
 
     
     public class Node {
+
         String item;
         Node next;
 
         public Node(String item) {
+
             this.item = item;
             this.next = null;
+
         }
     }
 
@@ -18,44 +21,67 @@ public class LinkedList {
     private int size;
 
     public LinkedList() {
+
         head = null;
         size = 0;
+
     }
 
     public void add(String item) {
+        
         Node newNode = new Node(item);
+
         if (head == null) {
+
             head = newNode;
+
         } else {
+
             Node curr = head;
+
             while (curr.next != null) {
+
                 curr = curr.next;
+
             }
+
             curr.next = newNode;
         }
+
         size++;
     }
 
     public void remove(String item) {
+
         if (head == null) {
+
             return;
+
         }
+
         if (head.item.equals(item)) {
+
             head = head.next;
             size--;
+
             return;
         }
+
         Node prev = head;
         Node curr = head.next;
+
         while (curr != null) {
+
             if (curr.item.equals(item)) {
                 prev.next = curr.next;
                 size--;
                 return;
             }
+
             prev = curr;
             curr = curr.next;
         }
+
     }
 
     public int getSize() {
@@ -63,10 +89,13 @@ public class LinkedList {
     }
 
     public void printList() {
+
         Node curr = head;
+
         while (curr != null) {
             System.out.println(curr.item);
             curr = curr.next;
+
         }
     }
 }
